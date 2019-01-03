2c1b2c63bd9bc17ab6c6663dcc22f3c5da410719
laizhiyuan <13423803799@163.com>
2019-01-03 17:35:10 +0800

    初始化项目

---
+++ src/main/java/com/lazy/security/LazySecurityApplication.java
@@ -0,0 +1,14 @@
+package com.lazy.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazySecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazySecurityApplication.class, args);
+	}
+
+}
+
